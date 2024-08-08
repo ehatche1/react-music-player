6a2a03016897570f73d0357345c8f09b22ed3ad7
ehatche1 <140868222+ehatche1@users.noreply.github.com>
2024-08-07 21:50:11 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>React Music Player</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://egypthportfolio.netlify.app/" target="_blank">
-          Egypt Hatchett{" "}
-        </a>
-      </footer>
+      <div className="container">
+        <h1>React Music Player</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://egypthportfolio.netlify.app/" target="_blank">
+            Egypt Hatchett{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/ehatche1/react-music-player"
+            target="_blank"
+          >
+            {" "}
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
